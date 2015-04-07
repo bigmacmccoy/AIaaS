17b0fae5ff10dbc95b1ee1ab12aae2aef1e8ea81
Braedon Big Mac McCoy <braedon.b.mccoy@gmail.com>
2015-04-07 17:17:18 +0000

    Updated response handling. Made it more robust and simpler.

--- driver.js
+++ driver.js
@@ -7,14 +7,13 @@ var https = require('https');
 
 var key = fs.readFileSync('./ssl/key.pem');
 var cert = fs.readFileSync('./ssl/cert.pem');
-var passport = require('passport');
 var crypto = require("crypto");
 
 var app = express();
 /* Uncomment for HTTPS once that gets sorted.
 var server = https.createServer({
-    key: key,
-    cert: cert
+	key: key,
+	cert: cert
 }, app).listen(process.env.PORT, process.env.IP);
 */
 // Comment out the next line once HTTPS functions.
@@ -25,244 +24,260 @@ var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var errorCode = 0;
-
 //app.listen(process.env.PORT, process.env.IP);
 
 
 /*
 Default: 
-    Params:
-    Function:
-        Displays a message based on if HTTPS works or not.
-    Returns:
-        HTTP Response Code
-        Message
+	Params:
+	Function:
+		Displays a message based on if HTTPS works or not.
+	Returns:
+		HTTP Response Code
+		Message
 */
 app.get('/', function(req, res){
-    if(req.secure){
-        res.send("Welcome to AI-as-a-Service!");
-    }else{
-        res.send("Your kind isn't welcome here!");
-    }
+	var returnObject = null;
+	if(req.secure){
+		 returnObject= {
+		  "ResponseCode":200,
+		  "Data":"Welcome to AI-as-a-Service!"
+		};
+	}else{
+		returnObject = {
+		  "ResponseCode":400,
+		  "Data":"Your kind is not welcome here!"
+		  };
+	}
+	res.status(returnObject.ResponseCode).send(returnObject.Data);
 });
 /*
 Get User Name: 
-    Params:
-        ID
-    Function:
-        Logs the Username for that ID.
-    Returns:
-        HTTP Response code
+	Params:
+		ID
+	Function:
+		Logs the Username for that ID.
+	Returns:
+		HTTP Response code
 */
 app.post('/getUserName', jsonParser, function (req, res) {
-    if(checkJSON(req.body, ["ID"])){ var connection = connect();
-        var connection = connect();
-        if(connection){
-            connection.query('SELECT * FROM `c9`.`Users` WHERE `id` = ?', [req.body.ID], function(err, results, fields) {
-                if (err){
-                    console.log(err);
-                    errorCode = 1;
-                }
-                if(results[0] != null){
-                    console.log(results[0].username);
-                    errorCode = 0;
-                }else{
-                    errorCode = 3;
-                }
-                disconnect(connection);
-            });
-        }
-    }else{
-        errorCode = 2;
-    }
-    return res.sendStatus(translateErrorCode(errorCode));
+	var returnObject = null;
+	if(checkJSON(req.body, ["ID"])){
+		var connection = connect();
+		if(connection){
+			connection.query('SELECT * FROM `c9`.`Users` WHERE `id` = ?', [req.body.ID], function(err, results, fields) {
+				if (err){
+					console.log(err);
+					returnObject = {
+					  "ResponseCode":400,
+					  "Data":"Error: " + err
+					};
+				}
+				if(results[0] != null){
+					console.log(results[0].username);
+					returnObject = {
+					  "ResponseCode":200,
+					  "Data":"Username: " + results[0].username
+					};
+				}else{
+					returnObject = {
+					  "ResponseCode":400,
+					  "Data":"No Results were returned."
+					};
+				}
+				disconnect(connection);
+				res.status(returnObject.ResponseCode).send(returnObject.Data);
+			});
+		}
+	}else{
+		returnObject = {
+		  "ResponseCode":400,
+		  "Data":"The necessary parameters were not sent."
+		};
+		res.status(returnObject.ResponseCode).send(returnObject.Data);
+	}
 });
 /*
 Login:
-    Params:
-        Username
-        Password
-    Function:
-        Checks username and password against DB.
-    Returns:
-        JSON containing the session token.
-    TODO:
-        Clean up how the app returns data. 
-        Write Auth using session token.
+	Params:
+		Username
+		Password
+	Function:
+		Checks username and password against DB.
+	Returns:
+		JSON containing the session token.
+	TODO:
+		Clean up how the app returns data. 
+		Write Auth using session token.
 */
 app.post('/login', jsonParser, function(req, res){
-    if(checkJSON(req.body, ["username", "password"])){
-        var connection = connect();
-        if(connection){
-            connection.query("SELECT * FROM `c9`.`Users` WHERE `username` = ?", [req.body.username], function(err, results, fields) {
-                if (err){
-                    console.log(err);
-                    errorCode = 1;
-                }
-                if(results[0] != null){
-                    if(req.body.password == results[0].password){
-                        var currentTime = results[0].lastAccess;
-                        var hmac = crypto.createHmac("sha1", currentTime.toString());
-                        hmac.setEncoding("hex");
-                        hmac.end(req.body.username, function(){
-                            var newToken = hmac.read();
-                            connection.query("UPDATE `c9`.`Users` SET `sessionToken` = ? WHERE `username` = ?", [newToken, req.body.username], function(err, results2, fields) {
-                                if (err){
-                                    console.log("1 " + err);
-                                    errorCode = 1;
-                                    return res.sendStatus(translateErrorCode(errorCode));
-                                }
-                            });
-                            connection.query("SELECT sessionToken FROM `c9`.`Users` WHERE username = ?", [req.body.username], function(err, results3, fields) {
-                                if (err){
-                                    console.log("2 " + err);
-                                    errorCode = 1;
-                                    return res.sendStatus(translateErrorCode(errorCode));
-                                }
-                                if(results3[0] != null){
-                                    errorCode = '{"SessionToken":"' + results3[0].sessionToken + '"}';
-                                    res.json(errorCode);
-                                }else{
-                                    errorCode = 2;
-                                    return res.sendStatus(translateErrorCode(errorCode));
-                                }
-                            });
-                        });
-                    }else{
-                        errorCode = 2;
-                        return res.sendStatus(translateErrorCode(errorCode));
-                    }
-                }else{
-                    errorCode = 3;
-                    return res.sendStatus(translateErrorCode(errorCode));
-                }
-            });
-        }
-        
-    }else{
-        errorCode = 2;
-        return res.sendStatus(translateErrorCode(errorCode));
-    }
+	var returnObject = null;
+	if(checkJSON(req.body, ["username", "password"])){
+		var connection = connect();
+		if(connection){
+			connection.query("SELECT * FROM `c9`.`Users` WHERE `username` = ?", [req.body.username], function(err, results, fields) {
+				if (err){
+					console.log(err);
+					returnObject = {
+						"ResponseCode":400,
+						"Data":"Error: " + err
+					};
+				}
+				if(results[0] != null){
+					if(req.body.password == results[0].password){
+						var currentTime = results[0].lastAccess;
+						var newToken = crypto.createHmac("sha1", currentTime.toString()).update(req.body.username).digest('hex');
+						connection.query("UPDATE `c9`.`Users` SET `sessionToken` = ? WHERE `username` = ?", [newToken, req.body.username], function(err, results2, fields) {
+							if (err){
+								returnObject = {
+									"ResponseCode":400,
+									"Data":"Error (Login Update): " + err
+								};
+							}
+						});
+						connection.query("SELECT sessionToken FROM `c9`.`Users` WHERE username = ?", [req.body.username], function(err, results3, fields) {
+							if (err){
+								returnObject = {
+									"ResponseCode":400,
+									"Data":"Error (Login Select): " + err
+								};
+							}
+							if(results3[0] != null){
+								returnObject = {
+									"ResponseCode":200,
+									"Data": results3[0].sessionToken
+								};
+							}else{
+								returnObject = {
+									"ResponseCode":400,
+									"Data":"Error: Select statement did not return anything."
+								};
+							}
+							res.status(returnObject.ResponseCode).send(returnObject.Data);
+						});
+					}else{
+						returnObject = {
+							"ResponseCode":400,
+							"Data":"Error: Passwords do not match!"
+						};
+						res.status(returnObject.ResponseCode).send(returnObject.Data);
+					}
+				}else{
+					returnObject = {
+						"ResponseCode":400,
+						"Data":"Error: No results were returned from the DB."
+					};
+					res.status(returnObject.ResponseCode).send(returnObject.Data);
+				}
+			});
+		}
+		
+	}else{
+		returnObject = {
+			"ResponseCode":400,
+			"Data":"Could not connect to DB."
+		};
+		res.status(returnObject.ResponseCode).send(returnObject.Data);
+	}
 });
 /*
 Create User: 
-    Params:
-        Username
-        Password
-        OS
-    Function:
-        Inserts the new user information into the DB
-    Returns:
-        HTTP Response code
+	Params:
+		Username
+		Password
+		OS
+	Function:
+		Inserts the new user information into the DB
+	Returns:
+		HTTP Response code
 */
 app.post("/createUser", jsonParser, function(req, res){
-    if(checkJSON(req.body, ["username", "password", "os"])){
-        var connection = connect();
-        if(connection){
-            connection.query("INSERT INTO `c9`.`Users`(`username`,`password`,`os`) VALUES(?,?,?)", [req.body.username, req.body.password, req.body.os], function(err, results, fields) {
-                if (err){
-                    console.log(err);
-                    errorCode = 1;
-                }
-                if(results[0] != null){
-                    console.log("Username: " + results[0].username);
-                }else{
-                    errorCode = 3;
-                }
-                disconnect(connection);
-            });
-        }
-        
-    }else{
-        errorCode = 2;
-    }
-    return res.sendStatus(translateErrorCode(errorCode));
+	var returnObject = {};
+	if(checkJSON(req.body, ["username", "password", "os"])){
+		var connection = connect();
+		if(connection){
+			connection.query("INSERT INTO `c9`.`Users`(`username`,`password`,`os`) VALUES(?,?,?)", [req.body.username, req.body.password, req.body.os], function(err, results, fields) {
+				if (err){
+					console.log(err);
+					returnObject = {
+						"ResponseCode":400,
+						"Data":"Error: " + err
+					};
+				}
+				returnObject = {
+					"ResponseCode":200,
+					"Data":"User has been created."
+				};
+				disconnect(connection);
+				res.status(returnObject.ResponseCode).send(returnObject.Data);
+			});
+		}
+		
+	}else{
+		returnObject = {
+			"ResponseObject":400,
+			"Data":"Error: Required parameters were not included."
+		};
+		res.status(returnObject.ResponseCode).send(returnObject.Data);
+	}
 });
 /*
 Check JSON: 
-    Params:
-        Body
-        Params
-    Function:
-        Checks that the required parameters are in the request.
-    Returns:
-        boolean value
+	Params:
+		Body
+		Params
+	Function:
+		Checks that the required parameters are in the request.
+	Returns:
+		boolean value
 */
 function checkJSON(body, params){
-    if (!body){
-        errorCode = 1;
-        return false;
-    } else {
-        for(var param in params){
-            var current = params[param];
-            if(body[current] == null){
-                errorCode = 2;
-            return false;
-            }
-        }
-        return true;
-    }
-}
-/*
-Translate Error Code: 
-    Params:
-        Error Code Value
-    Function:
-        Translates an error code value into an HTTP error code
-    Returns:
-        HTTP Response code number
-    TODO:
-        Do something better with how we return things. We need to support JSON return objects.
-*/
-function translateErrorCode(errorCode){
-    switch(errorCode){
-        case 0: // Everything is copacetic.
-            return 200;
-        case 1: // Testing error or the code isn't written yet.
-            return 418;
-        case 2: // You tried to do something you shouldn't have
-            return 400;
-        case 3: // No response for you. Bad boy.
-            return 403;
-        default: // You must be returning something else...
-            return errorCode;
-    }
+	if (!body){
+		return false;
+	} else {
+		for(var param in params){
+			var current = params[param];
+			if(body[current] == null){
+				return false;
+			}
+		}
+	}
+	return true;
 }
 /*
 Connect: 
-    Params:
-        
-    Function:
-        Connects the app to the DB
-    Returns:
-        Connection object
+	Params:
+		
+	Function:
+		Connects the app to the DB
+	Returns:
+		Connection object
 */
 function connect(){
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'bigmacmccoy',
-        password : ''
-    });
+	var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'bigmacmccoy',
+		password : ''
+	});
 
-    connection.connect();
-    return connection;
+	connection.connect();
+	return connection;
 }
 /*
 Disconnect: 
-    Params:
-        Connection object
-    Function:
-        Disconnects the app from the DB
-    Returns:
-    
+	Params:
+		Connection object
+	Function:
+		Disconnects the app from the DB
+	Returns:
+	
 */
 function disconnect(connection){
-    connection.end();
+	connection.end();
 }
 
 function getMYSQLTime(){
-    var currentTime = new Date();
-    currentTime = currentTime.getUTCFullYear() + '-' +  ('00' + (currentTime.getUTCMonth()+1)).slice(-2) + '-' + ('00' + currentTime.getUTCDate()).slice(-2) + ' ' + ('00' + currentTime.getUTCHours()).slice(-2) + ':' + ('00' + currentTime.getUTCMinutes()).slice(-2) + ':' + ('00' + currentTime.getUTCSeconds()).slice(-2);
-    return currentTime;
+	var currentTime = new Date();
+	currentTime = currentTime.getUTCFullYear() + '-' +  ('00' + (currentTime.getUTCMonth()+1)).slice(-2) + '-' + ('00' + currentTime.getUTCDate()).slice(-2) + ' ' + ('00' + currentTime.getUTCHours()).slice(-2) + ':' + ('00' + currentTime.getUTCMinutes()).slice(-2) + ':' + ('00' + currentTime.getUTCSeconds()).slice(-2);
+	return currentTime;
 }
